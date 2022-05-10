75530dcdd16db17b62f48d5883390543c444ef9a
Agon Ademaj <agonademaj@aol.com>
2022-05-09 14:18:34 -1200

    FrontEnd: Created the initial app

---
+++ Client/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="container">
+      <header className="header">
+        <h1 style={{color: "green"}}>SPARTan Football Manage</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
