b0282b4a22d512f5904e0e8009d974f555e0b205
ermalMorina <ermalmorina2000@gmail.com>
2022-05-11 14:16:54 +0200

    feat: Models and db hosted

---
+++ Backend/SpartanManageFootball/SpartanManageFootball/Models/Match.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpartanManageFootball.Models
+{
+    public class Match
+    {
+        [Key]
+        public int MatchId { get; set; }
+        [Required]
+        public Squad EkipiVendas { get; set; }
+        [Required] 
+        public Squad EkipiMusafir { get; set; }
+
+        [Required] 
+        public List<Referee> Referees { get; set; }
+        [Required] 
+        public DateTime MatchDate { get; set; }
+    }
+}
