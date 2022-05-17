0e90d37e89843bf013b1f7985a8ccdb4257a9ede
Agon <68818772+agon-ademaj@users.noreply.github.com>
2022-05-17 13:08:06 +0200

    Update App.js
    Removed the extra lines

--- Client/src/App.js
+++ Client/src/App.js
@@ -10,9 +10,7 @@ import Teams from './components/teams';
 import About from './components/about';
 import Login from './components/login';
 import Register from './components/register';
-
 import Match from './components/match';
-
 
 function App() {
   return (
