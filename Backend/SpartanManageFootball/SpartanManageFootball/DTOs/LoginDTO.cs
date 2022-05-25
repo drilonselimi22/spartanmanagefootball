4d5678059e2fb2c1bf689e81ce3933038f28ca8c
ermalMorina <ermalmorina2000@gmail.com>
2022-05-25 10:25:39 +0200

    feature/SMF-15

---
+++ Backend/SpartanManageFootball/SpartanManageFootball/DTOs/LoginDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace SpartanManageFootball.DTOs
+{
+    public class LoginDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Token { get; set; }
+    }
+}
---
+++ Backend/SpartanManageFootball/SpartanManageFootball/Services/IdentityServices.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SpartanManageFootball.Interfaces;
+
+namespace SpartanManageFootball.Services
+{
+    public class IdentityServices : IIdentityService
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public IdentityServices(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+
+
+
+        public async Task<(string id, string roleName)> GetRoleByIdAsync(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            return (role.Id, role.Name);
+        }
+
+        public async Task<List<(string id, string roleName)>> GetRolesAsync()
+        {
+            var roles = await _roleManager.Roles.Select(x => new
+            {
+                x.Id,
+                x.Name
+            }).ToListAsync();
+
+            return roles.Select(role => (role.Id, role.Name)).ToList();
+        }
+
+        public async Task<(string userId, string fullName, string UserName, string email, IList<string> roles)> GetUserDetailsAsync(string userId)
+        {
+            var user = await _userManager.Users.FirstOrDefaultAsync(x => x.Id == userId);
+            if (user == null)
+            {
+                throw new Exception("User not found");
+            }
+            var roles = await _userManager.GetRolesAsync(user);
+            return (user.Id, user.UserName, user.UserName, user.Email, roles);
+        }
+        public async Task<string> GetUserIdAsync(string userName)
+        {
+            var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == userName);
+            if (user == null)
+            {
+                throw new Exception("User not found");
+                //throw new Exception("User not found");
+            }
+            return await _userManager.GetUserIdAsync(user);
+        }
+        public async Task<bool> SigninUserAsync(string email, string password)
+        {
+            var result = await _signInManager.PasswordSignInAsync(email, password, true, false);
+            return result.Succeeded;
+
+
+        }
+    }
+}
