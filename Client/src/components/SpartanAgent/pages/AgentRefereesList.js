18dfa8c85cd1f9f8ffbd5b3c9837e7064365bc63
Agon <68818772+agon-ademaj@users.noreply.github.com>
2022-06-24 10:30:33 +0200

    Small changes in features (#45)

--- Client/src/components/SpartanAgent/pages/AgentRefereesList.js
+++ Client/src/components/SpartanAgent/pages/AgentRefereesList.js
@@ -2,19 +2,19 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import PaginatedItems from "./PaginationReferees/PaginatedItems";
 export default function AgentRefereesList() {
-  const [referees, setReferees] = useState([]); 
+  const [referees, setReferees] = useState([]);
 
   const getData = async () => {
     console.log("fore calling ")
     const { data } = await axios.get("https://localhost:7122/api/Referees");
     setReferees(data);
-    console.log("data",data)
-    console.log("referees",referees)
+    console.log("data", data)
+    console.log("referees", referees)
   };
 
   useEffect(() => {
     getData();
-  }, []); 
+  }, []);
 
   return (
     <div>
